461fa3c47f52d693e43c8d26a38effee548dc034
Jeremy Winnett <winnettjeremy@gmail.com>
2025-04-19 19:44:12 -0400

    figured out tempConversion test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,30 @@
-const convertToCelsius = function() {
+
+
+
+
+
+
+const convertToCelsius = function(fahrenheit) {
+
+
+  const celsius = (fahrenheit - 32) * .555555556;
+
+  return Math.round(celsius * 10) / 10;
+    
 };
 
-const convertToFahrenheit = function() {
+
+
+
+const convertToFahrenheit = function(celsius) {
+
+  const fahrenheit = (celsius * 9/5) + 32;
+
+  return Math.round(fahrenheit * 10) / 10;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = {
