bd765f691357b1d5ddfc5a781610e65def67832d
Jeremy Winnett <winnettjeremy@gmail.com>
2025-05-20 18:41:56 -0400

    wrote code to convert to an array of book titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
 
+
+const getTheTitles = function(books) {
+    return books.map(books => books.title);
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+
+
+//           npm test getTheTitles.spec.js
