aa163882132b3b7f58398bf4e7dac7c6b9001a88
Jeremy Winnett <winnettjeremy@gmail.com>
2025-05-20 18:05:39 -0400

    completed with no errors 08, 09, and 10

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	const num = a + b;
+  return num;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	const num = a - b;
+  return num;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum = array.reduce((total, num) => total + num, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const product = array.reduce((total, num) => total * num, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+  if (fact < 0) return "Undefined";
+  let num = 1;
+  for (let i = 1; i <= fact; i++) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
 
-};
+    if (!Number.isInteger(fib)) {
+        Error("OOPS");
+    }
+
+    if (fib < 0) {
+        return ("OOPS");
+    }
+
+    if (fib == 0) {
+        return 0;
+    }
+    if (fib === 1 || fib === 2) {
+        return 1;
+    }
+    let prev = 1, curr = 1;
+
+    for (let i = 3; i <= fib; i++) {
+        const next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+
+    return curr;
+
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
